cf0f84bb8527c43cd304883bf78cb88e2301c42a
FD <piratrrr@gmail.com>
2023-06-27 14:06:28 +0500

    Убраны принты

--- HydroTrade/media/lua/server/SafeHouseTimeLeft.lua
+++ HydroTrade/media/lua/server/SafeHouseTimeLeft.lua
@@ -3,19 +3,14 @@ local daysUntilDeletion = getServerOptions():getInteger("SafeHouseRemovalTime")
 
 local function checkSafehouses()
     local currentTime = getTimeInMillis()
-    print("daysUntilDeletion:",daysUntilDeletion) --240
-    print("currentTime:",currentTime) --  1687855316931
     local safehouses = SafeHouse.getSafehouseList()
     for i = 0, safehouses:size() - 1 do
         local safehouse = safehouses:get(i)
         if safehouse then
             local lastVisitedTimestamp = safehouse:getLastVisited()
-            print("lastVisitedTimestamp:",lastVisitedTimestamp) --1687245174202
             local lastVisitedDate = os.date("%Y-%m-%d %H:%M:%S", lastVisitedTimestamp / 1000)
-
             local timeLeftMillis = lastVisitedTimestamp + (daysUntilDeletion/24 * dayInMillis) - currentTime
             local daysRemaining = math.floor(timeLeftMillis / (dayInMillis))
-            print("daysRemaining:",daysRemaining .. " Owner:"..tostring(safehouse:getOwner())) -- -3
             if daysRemaining <= 1 then
                 local timeLeftMillis = lastVisitedTimestamp + (daysUntilDeletion/24 * dayInMillis) - currentTime
                 local timeLeftDays = math.floor(timeLeftMillis / (dayInMillis))
