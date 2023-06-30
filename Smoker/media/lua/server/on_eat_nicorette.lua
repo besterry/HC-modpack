988fcc2c548f3dd4d2066a386e13bf118ab8dd4a
FD <piratrrr@gmail.com>
2023-06-30 23:44:43 +0500

    Fix nicorette

--- Smoker/media/lua/server/on_eat_nicorette.lua
+++ Smoker/media/lua/server/on_eat_nicorette.lua
@@ -1,6 +1,6 @@
 function OnEat_Nicorette(food, character, percent)
 	if food:getType() == "SMGum" then
-		local player = getSpecificPlayer(0);
+		local player = character
 		local DATAPlayer = player:getModData();
 		DATAPlayer.OnEat_Nicorette = true;
 		DATAPlayer.OnEat_Nicorette_Time = 0;
