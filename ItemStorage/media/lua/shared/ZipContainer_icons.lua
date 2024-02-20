a0c555c7abbd3f335699882c55d00316bd644dfe
FD <piratrrr@gmail.com>
2024-02-21 00:24:46 +0500

    Добавлена иконка ящику

--- ItemStorage/media/lua/shared/ZipContainer_icons.lua
+++ ItemStorage/media/lua/shared/ZipContainer_icons.lua
@@ -1,4 +1,6 @@
 ContainerButtonIcons = ContainerButtonIcons or {}
 local iconTex = getTexture("media/ZipContainer_icon.png")
+local iconTex2 = getTexture("media/ZipContainerBox_icon.png")
 
 ContainerButtonIcons.ZipContainer = iconTex
+ContainerButtonIcons.ZipContainerBox = iconTex2
