5a547c94a4bf2861ec550d94b68bf06f8fb6763a
FD <piratrrr@gmail.com>
2024-02-21 01:18:22 +0500

    Fix отображение F1 во время собирательства

--- HydroTrade/media/lua/client/SurvivalGuide/HooksetShowFirstTimeSearchTutorial.lua
+++ HydroTrade/media/lua/client/SurvivalGuide/HooksetShowFirstTimeSearchTutorial.lua
@@ -1,10 +1,10 @@
 local old_checkShowFirstTimeSearchTutorial = ISSearchWindow.checkShowFirstTimeSearchTutorial
 
 function ISSearchWindow:checkShowFirstTimeSearchTutorial()
-    local o = old_checkShowFirstTimeSearchTutorial()
-    if getCore():isShowFirstTimeSearchTutorial() then
-        getCore():setShowFirstTimeSearchTutorial(false);
-		getCore():saveOptions();
-    end
+    local o = old_checkShowFirstTimeSearchTutorial(self)
+    --if getCore():isShowFirstTimeSearchTutorial() then        
+    getCore():setShowFirstTimeSearchTutorial(false);
+    getCore():saveOptions();
+    --end
     return o
 end
