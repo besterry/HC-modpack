48b746b2a2f41397827138f970b22033a6ea3b68
FD <piratrrr@gmail.com>
2024-02-21 23:48:19 +0500

    обновлен рецепт, админам не нужна карта

--- itemGPS/media/lua/client/itemGPSmod/optionnal features/Hook_ISWorldMapGPS_NoToggleMap.lua
+++ itemGPS/media/lua/client/itemGPSmod/optionnal features/Hook_ISWorldMapGPS_NoToggleMap.lua
@@ -6,8 +6,8 @@ Events.OnGameStart.Add(function()
 		if not ISWorldMap.IsAllowed() or (ISWorldMap.IsAllowed() and noMap == 0 and not isAdmin()) then
 			getSpecificPlayer(0):Say(getText("IGUI_No_Map_IG"))
 			return
-		end --and not isAdmin()
-		if noMap == 1 and not itemGPSmod.gps and not wmap then getSpecificPlayer(0):Say(getText("IGUI_No_GPS_on")) return end
+		end
+		if noMap == 1 and not itemGPSmod.gps and not wmap and not isAdmin() then getSpecificPlayer(0):Say(getText("IGUI_No_GPS_on")) return end
 		
 		ISWorldMap_ToggleWorldMap_Origin(playerNum)
 	end	
