d75ffb6b07c56af010f8f3832de6916d5ec0ffee
FD <piratrrr@gmail.com>
2024-02-27 15:58:07 +0500

    fix gametime

--- Online/media/lua/server/Online.lua
+++ Online/media/lua/server/Online.lua
@@ -12,8 +12,10 @@ end
 local function PlayersOnline()
     --Получение игрового времени
     local hour = tostring(getGameTime():getHour())
-    local mimutes = tostring(getGameTime():getMinutes())
-    local gameTime = hour .. ":" .. mimutes
+    local mimutes = string.format("%02d",(tostring(getGameTime():getMinutes())))
+    print(getGameTime():getMinutes())
+    local gameTime = tostring(hour .. ":" .. mimutes)
+    print("gameTime:",gameTime)
     --Получение количества игроков и запись в строку result и игрового времени
     local maxPlayersOption = getServerOptions():getOptionByName("MaxPlayers"):getValue()
     local players = getOnlinePlayers():size()  
