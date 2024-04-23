e7de0caaae2ea79a1a213ad375f65cf9c8572f8e
FD <piratrrr@gmail.com>
2024-04-23 13:38:21 +0500

    fix fitness

--- HydroTrade/media/lua/client/Fix_ISFitnessUIupdateButtons.lua
+++ HydroTrade/media/lua/client/Fix_ISFitnessUIupdateButtons.lua
@@ -2,8 +2,7 @@ local oldFunc = ISFitnessUI.updateButtons
 ---@diagnostic disable-next-line: duplicate-set-field
 function ISFitnessUI:updateButtons(currentAction)
 	oldFunc(self, currentAction)
-
-	if self.player:isPlayerMoving() or self.player:pressedMovement(false) then
+	if self.player:isPlayerMoving()  then -- or self.player:pressedMovement(false)
 		self.ok.enable = false;
 	end
 end
