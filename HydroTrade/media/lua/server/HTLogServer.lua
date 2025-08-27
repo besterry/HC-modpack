3832bae011a3b58f7b340167b1018067d3480e4f
besterry <piratrrr@gmail.com>
2025-08-28 01:09:17 +0500

    Добавлены функции логирования для ванильного ремонта автомобилей и деталей

--- HydroTrade/media/lua/server/HTLogServer.lua
+++ HydroTrade/media/lua/server/HTLogServer.lua
@@ -44,6 +44,31 @@ commands.setContainerContentAmount = function (player, args) -- args = { vehicle
     writeLog("vehicle-FuelChange", msg)
 end
 
+--sendClientCommand(playerObj, "vehicle", "repairPart", { vehicle = part:getVehicle():getId(), part = part:getId() })
+commands.repairPart = function(player, args) -- args = { vehicle = self.vehicle:getId(), part = self.part:getId() }
+    local vehicle = getVehicleById(args.vehicle)
+    local part = vehicle:getPartById(args.part)
+    if not part then
+        return
+    end
+    local msg = '"' .. player:getUsername() .. '"'.. " -> REPAIR PART TO 100%" .. " [" .. math.floor(player:getX()) .. "," ..  math.floor(player:getY()) .. ",0]" ..
+    " vehicle: " .. vehicle:getScriptName() .. "[" .. math.floor(vehicle:getX()) .. "," .. math.floor(vehicle:getY()) .. ",0]" ..
+    " SqlId: " .. vehicle:getModData().sqlId .. " Part: " .. part:getId() 
+    writeLog("admin", msg)
+end
+
+-- sendClientCommand(playerObj, "vehicle", "repair", { vehicle = vehicle:getId() })
+commands.repair = function(player, args) -- args = { vehicle = self.vehicle:getId() }
+    local vehicle = getVehicleById(args.vehicle)
+    if not vehicle then
+        return
+    end    
+    local msg = '"' .. player:getUsername() .. '"' .. " -> REPAIR VEHICLE TO 100%" .. " [" .. math.floor(player:getX()) .. "," .. math.floor(player:getY()) .. ",0]" ..
+    " vehicle: " .. vehicle:getScriptName() .. "[" .. math.floor(vehicle:getX()) .. "," .. math.floor(vehicle:getY()) .. ",0]" ..
+    " SqlId: " .. (vehicle:getModData().sqlId or "N/A")    
+    writeLog("admin", msg)
+end
+
 
 local function BalanceAndSH_OnClientCommand(module, command, player, args)
     if module == "vehicle" and commands[command] then
