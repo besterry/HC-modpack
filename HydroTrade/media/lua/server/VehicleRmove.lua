614714e1accd8455403ecec5d0bd9f29fc2ba453
besterry <piratrrr@gmail.com>
2025-09-06 21:30:26 +0500

    Удален файл VehicleRmove.lua, содержащий функцию обработки команды rmove для удаления транспортного средства.

--- HydroTrade/media/lua/server/VehicleRmove.lua
+++
@@ -1,7 +0,0 @@
-if isClient() then return end
-local Rmove = function(module, command, player, args)
-    if module == 'vehicle' and command == "rmove" then
-        triggerEvent("OnClientCommand", module, "remove", player, args)
-    end
-end
-Events.OnClientCommand.Add(Rmove)
