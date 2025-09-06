f51e3c387d545d4846de582d8ae442e7a60bb558
besterry <piratrrr@gmail.com>
2025-09-06 21:30:11 +0500

    Добавлена функция логирования удаления транспортного средства при выполнении команды rmove.

---
+++ HydroTrade/media/lua/server/Cars/VehicleRmove.lua
@@ -0,0 +1,14 @@
+if isClient() then return end
+
+local Rmove = function(module, command, player, args)
+    if module == 'vehicle' and command == "rmove" then
+        print("Rmove")
+        local vehicle = getVehicleById(args.vehicle)
+        if vehicle then -- Если машина найдена, то логируем
+            local msg = player:getUsername() ..  " Rmove: " ..  vehicle:getScriptName() .. " [" .. math.floor(vehicle:getX()) .. "," .. math.floor(vehicle:getY()) .. ",0]" .. " server SqlId: " .. vehicle:getSqlId()
+            writeLog("vehicle", msg)
+        end
+        triggerEvent("OnClientCommand", module, "remove", player, args)
+    end
+end
+Events.OnClientCommand.Add(Rmove)
