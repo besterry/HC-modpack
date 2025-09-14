45552d7742a6527c48dd7c5defbbb2b23686d817
besterry <piratrrr@gmail.com>
2025-09-15 02:07:15 +0500

    Добавлена функция получения данных модификаций игрока и обработка команд сервера для отображения модификаций. Обновлен контекстное меню для отображения данных модификаций в зависимости от выбранного игрока.

--- HydroTrade/media/lua/client/Admins/SeeModDataWorld.lua
+++ HydroTrade/media/lua/client/Admins/SeeModDataWorld.lua
@@ -4,6 +4,12 @@ local function showModdata(obj)
     ItemModDataPanel.OnOpenPanel(obj)
 end
 
+local function getPlayerModdata(player)
+    print("Send client command")
+    local args = { player = player:getUsername() }
+    sendClientCommand("HT_GMD", "getPlayerModdata", args)
+end
+
 local function ContextMenu(player, context, worldobjects, test)
     if not isAdmin() then return; end
 	local sq = nil
@@ -68,14 +74,21 @@ local function ContextMenu(player, context, worldobjects, test)
                             local corpse = sq2:getStaticMovingObjects():get(i)
                             if corpse:hasModData() then
                                 local moddata = corpse:getModData()
+                                local killer = ""
+                                local killerTime = ""
                                 if moddata.zombieKilled then
-                                    local killer = "[" .. moddata.zombieKilled .. "]"
+                                    killer = "[" .. moddata.zombieKilled .. "]"
                                 else
                                     killer = ""
                                 end
                                 if moddata.zombieKilledTime then
                                     local currentTime = getGameTime():getWorldAgeHours()
-                                    local killerTime = math.floor((currentTime - moddata.zombieKilledTime)*0.125) .. "h" -- 0.125 - время суток 3/24 (можно получить из настроек сервера)
+                                    local time = (currentTime - moddata.zombieKilledTime)*0.125 --0.125 - время суток 3/24 (можно получить из настроек сервера)
+                                    if time < 1 then
+                                        killerTime = math.floor(time*60) .. "m ago" -- минуты
+                                    else
+                                        killerTime = math.floor(time) .. "h ago" -- часы
+                                    end
                                 else
                                     killerTime = ""
                                 end
@@ -91,19 +104,49 @@ local function ContextMenu(player, context, worldobjects, test)
             end
         end
         if clickedPlayer then
-            context:addOption(getText("IGUI_Show_player_modData") .. " " .. clickedPlayer:getUsername(), clickedPlayer, showModdata)
+            if clickedPlayer == getPlayer() then
+                context:addOption(getText("IGUI_Show_player_modData") .. " " .. clickedPlayer:getUsername(), clickedPlayer, showModdata)
+            else
+                context:addOption(getText("IGUI_Show_player_modData") .. " " .. clickedPlayer:getUsername(), clickedPlayer, getPlayerModdata)
+            end
         end
     end
 end
-
 Events.OnFillWorldObjectContextMenu.Add(ContextMenu);
 
-
-Events.OnZombieDead.Add(function(zombie)
-    local moddata = zombie:getModData()
-    local killer = getPlayer():getUsername()
-    if killer then
-        moddata.zombieKilled = killer
-        moddata.zombieKilledTime = getGameTime():getWorldAgeHours()
+local function OnServerCommand(module, command, args)
+    if module == "HT_GMD" and command == "onShowModdata" then
+        local obj = args.moddata
+        if obj then
+            ItemModDataPanel.OnOpenPanel(obj)
+        end
     end
+end
+Events.OnServerCommand.Add(OnServerCommand)
+
+--[[ не работает transmitModData на клиенте (не отправляется на сервер)
+Events.OnZombieDead.Add(function(zombie) -- При смерти зомби записываем данные о смерти
+    -- Ждем пока зомби изменит состояние на труп
+    local inv = zombie:getInventory()
+    if not inv then return end
+    local waitCorpseCount = 0
+	local function waitCorpse() -- Ожидание появления тела
+		local parent = inv:getParent()
+		if parent and instanceof(parent, "IsoDeadBody") then
+            waitCorpseCount = waitCorpseCount + 1
+            Events.OnTick.Remove(waitCorpse)
+            local moddata = parent:getModData()
+            local killer = getPlayer():getUsername()
+            if killer then
+                moddata.zombieKilled = killer
+                moddata.zombieKilledTime = getGameTime():getWorldAgeHours()
+                parent:transmitModData()
+            end
+        end
+        if waitCorpseCount >= 30 then -- На всякий случай, если что-то пошло не так отпишемся, чтоб не повисло
+            Events.OnTick.Remove(waitCorpse)
+        end
+    end
+    Events.OnTick.Add(waitCorpse)
 end);
+]]
