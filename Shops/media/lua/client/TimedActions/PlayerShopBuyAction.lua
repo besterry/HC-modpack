298baf8acaf8be03895d99a6809fc70be58ad126
besterry <piratrrr@gmail.com>
2025-09-18 02:08:00 +0500

    Добавлена логика обработки ордеров на скупку и улучшено логирование покупок в магазине.

--- Shops/media/lua/client/TimedActions/PlayerShopBuyAction.lua
+++ Shops/media/lua/client/TimedActions/PlayerShopBuyAction.lua
@@ -1,4 +1,5 @@
 require "TimedActions/ISBaseTimedAction"
+PSClient = PSClient or {}
 local Nfunction = require "Nfunction"
 PlayerShopBuyAction = ISBaseTimedAction:derive("PlayerShopBuyAction")
 
@@ -31,23 +32,80 @@ function PlayerShopBuyAction:perform()
     local playerInv = self.character:getInventory()
     local total = 0
     local totalSpecial = 0
-    for k,v in pairs(cartItems) do
-        local item = v.item
-        local invItem = item.invItem
-        shopContainer = self.shop:getContainer()
-        if shopContainer:contains(invItem) then
-            shopContainer:Remove(invItem)
-            shopContainer:removeItemOnServer(invItem)
-            playerInv:addItem(invItem)
-            if SandboxVars.Shops.PurchaseLog then Nfunction.buildLogShop(invItem:getFullType()) end
-            if item.specialCoin then
-                totalSpecial = totalSpecial + item.price
+    local itemsSold = {}
+    local soldUnits = 0 -- сколько единиц продали в магазин (скупка)
+    local sellTotalCoin, sellTotalSpec = 0, 0
+    -- 1) Сначала обработаем все ордера на скупку агрегировано по ключу
+    local orderBatches = {}
+    for _,v in pairs(cartItems) do
+        local it = v.item
+        if it.orderKey then
+            local key = it.orderKey
+            local units = it.units or 1
+            if not orderBatches[key] then
+                orderBatches[key] = { type = it.type, units = units }
             else
-                total = total + item.price
+                orderBatches[key].units = orderBatches[key].units + units
             end
-            local modData = invItem:getModData()
-            modData.specialCoin = nil
-            modData.price = nil
+        end
+    end
+    for _,batch in pairs(orderBatches) do
+        local units = batch.units or 0
+        if units > 0 then
+            local sample = playerInv:FindAndReturn(batch.type)
+            if sample then
+                local cont = self.shop:getContainer()
+                local unitWeight = sample:getActualWeight()
+                local maxByWeight = math.floor(math.max(0, (cont:getCapacity() - cont:getCapacityWeight())) / math.max(0.0001, unitWeight))
+                local allowed = math.min(units, maxByWeight)
+                if allowed <= 0 then
+                    self.character:setHaloNote(getText("IGUI_ContainerFull"), 255,255,255,300)
+                    return -- выходим из всей функции если контейнер заполнен
+                else
+                    local removed = 0
+                    while removed < allowed do
+                        local it = playerInv:FindAndReturn(batch.type)
+                        if not it then break end
+                        playerInv:Remove(it)
+                        if SandboxVars.Shops.PurchaseLog then Nfunction.buildLogShop(batch.type) end
+                        removed = removed + 1
+                    end
+                    if removed > 0 then
+                        soldUnits = soldUnits + removed
+                        local shopSquare = self.shop:getSquare()
+                        local coords = {x = shopSquare:getX(), y = shopSquare:getY(), z = shopSquare:getZ()}
+                        local paid = PSClient.SellToBuyOrder(self.character, {coords = coords, itemType = batch.type, quantity = removed})
+                        if paid then
+                            sellTotalCoin = sellTotalCoin + (paid.coin or 0)
+                            sellTotalSpec = sellTotalSpec + (paid.special or 0)
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    -- 2) Затем обработаем обычные покупки из магазина
+    for _,v in pairs(cartItems) do
+        local item = v.item
+        if not item.orderKey then
+            local invItem = item.invItem
+            shopContainer = self.shop:getContainer()
+            if shopContainer:contains(invItem) then
+                shopContainer:Remove(invItem)
+                shopContainer:removeItemOnServer(invItem)
+                playerInv:addItem(invItem)
+                if SandboxVars.Shops.PurchaseLog then Nfunction.buildLogShop(invItem:getFullType()) end
+                if item.specialCoin then
+                    totalSpecial = totalSpecial + item.price
+                else
+                    total = total + item.price
+                end
+                local modData = invItem:getModData()
+                modData.specialCoin = nil
+                modData.price = nil
+                table.insert(itemsSold, {type = invItem:getFullType(), name = invItem:getName(), price = item.price, specialCoin = item.specialCoin})
+            end
         end
     end
     local shopSquare = self.shop:getSquare()
@@ -56,14 +114,18 @@ function PlayerShopBuyAction:perform()
         y = shopSquare:getY(),
         z = shopSquare:getZ(),
     }
-    if SandboxVars.Shops.PurchaseLog then Nfunction.logShop(coords,"Purchase") end
+    if SandboxVars.Shops.PurchaseLog then
+        if soldUnits > 0 then
+            Nfunction.logShop(coords, "Sell (" .. self.shop:getModData().owner .. ") [price: " .. sellTotalCoin .. "c/" .. sellTotalSpec .. "s]")
+        else
+            Nfunction.logShop(coords, "Purchase (" .. self.shop:getModData().owner .. ") [price: " .. total .. "c/" .. totalSpecial .. "s]")
+        end
+    end
     self.character:playSound("CashRegister")
     local income = self.shop:getModData().income
-    local data = {
-        b = self.character:getUsername(),
-        t = {tl = total, tls = totalSpecial}
-    }
+    local data = { b = self.character:getUsername(), t = {tl = total, tls = totalSpecial}, items = itemsSold }
     if total > 0 or totalSpecial > 0 then
+        -- обычная покупка: списываем со счета игрока и пишем доход магазина
         sendClientCommand("BS", "Withdraw", {total,totalSpecial})
         table.insert(income,data)
     end
