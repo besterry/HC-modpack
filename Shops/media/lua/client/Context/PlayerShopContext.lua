a391be07700640e43d7ac662b42ba61b2f7b6988
besterry <piratrrr@gmail.com>
2025-09-19 19:22:08 +0500

    Добавлена проверка наличия средств в магазине перед подъемом предметов, улучшено отображение уведомлений для игрока.

--- Shops/media/lua/client/Context/PlayerShopContext.lua
+++ Shops/media/lua/client/Context/PlayerShopContext.lua
@@ -83,6 +83,11 @@ function PlayerShop.PickupShop(worldobjects,player,shop) -- Поднимаем �
         player:setHaloNote(UIText.RemoveIncomePlayerShop, 255,255,255,400);
         return
     end
+    local cash = shop:getModData().cash
+    if cash and cash.coin > 0 or cash.specialCoin > 0 then
+        player:setHaloNote(UIText.RemoveCashPlayerShop, 255,255,255,400);
+        return
+    end
     shop:getSquare():transmitRemoveItemFromSquare(shop)
     local item = "Base.PlayerShop"
     if shop:getContainer():getType() == "freezer" then
