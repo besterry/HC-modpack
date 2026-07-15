2b11138eae7de8cd51b99b1ff4746f0b4f7e490c
besterry <piratrrr@gmail.com>
2026-07-16 01:21:16 +0500

    Refactor vehicle part installation and uninstallation logging by moving it to VehicleCommands.lua. This change centralizes logging functionality and improves code organization for vehicle-related commands.

--- HydroTrade/media/lua/server/HTLogServer.lua
+++ HydroTrade/media/lua/server/HTLogServer.lua
@@ -1,35 +1,6 @@
 local commands = {}
 
-commands.installPart  = function(player, args) -- args = { vehicle = self.vehicle:getId(), part = self.part:getId(), item = self.item, perks = perksTable, mechanicSkill = self.character:getPerkLevel(Perks.Mechanics) }
-    local vehicle = getVehicleById(args.vehicle)
-    local part = vehicle:getPartById(args.part)
-    local item = args.item
-    local msg = "Player: " .. player:getUsername() ..  " [" .. math.floor(player:getX()) .. "," .. math.floor(player:getY()) .. ",0]" ..
-    " vehicle: " ..  vehicle:getScriptName() .. "[" .. math.floor(vehicle:getX()) ..  "," .. math.floor(vehicle:getY()) .. ",0]" ..
-    " SqlId: " .. vehicle:getModData().sqlId ..
-    " Install part: " .. part:getId() .. " -> Item:" .. item:getFullType() .. ", Condition:" .. args.item:getCondition()
-    writeLog("vehicle-Parts", msg)
-end
-
-commands.uninstallPart  = function(player, args) --args = { vehicle = self.vehicle:getId(), part = self.part:getId(), perks = perksTable, mechanicSkill = self.character:getPerkLevel(Perks.Mechanics), contentAmount = self.part:getContainerContentAmount() }
-    local vehicle = getVehicleById(args.vehicle)
-    local part = vehicle:getPartById(args.part)
-    if not part then
-        return
-    end
-    local item = part:getInventoryItem()
-    local itemMsg = ""
-    if not item then
-        itemMsg = " Condition:" .. part:getCondition()
-    else
-        itemMsg = " -> Item:" .. item:getFullType() .. ", Condition:" .. item:getCondition()
-    end
-    local msg = "Player: " .. player:getUsername() .. " [" .. math.floor(player:getX()) .. "," ..  math.floor(player:getY()) .. ",0]" ..
-    " vehicle: " .. vehicle:getScriptName() .. "[" .. math.floor(vehicle:getX()) .. "," .. math.floor(vehicle:getY()) .. ",0]" ..
-    " SqlId: " .. vehicle:getModData().sqlId ..
-    " Uninstall part: " .. part:getId() .. itemMsg
-    writeLog("vehicle-Parts", msg)
-end
+-- installPart / uninstallPart logging moved to Vehicles/VehicleCommands.lua (logs after roll with Result)
 
 commands.setContainerContentAmount = function (player, args) -- args = { vehicle = self.vehicle:getId(), part = self.part:getId(), amount = self.tankTarget }
     local vehicle = getVehicleById(args.vehicle)
